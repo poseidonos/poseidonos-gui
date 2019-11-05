e2a99b97f99590cdefb76df9134ee49b3a5074ed
d.moon <d.moon@samsung.com>
2019-11-05 16:21:19 +0900

    Fixed

--- server/setting/setting.go
+++ server/setting/setting.go
@@ -20,8 +20,8 @@ type ConfigScheme struct {
 
 type Server struct {
 	Dagent HostConf `yaml:"dagent"`
-	IBoF   HostConf `yaml:"iBoF"`
-	BMC   HostConf `yaml:"BMC"`
+	IBoF   HostConf `yaml:"ibof"`
+	BMC   HostConf `yaml:"bmc"`
 }
 type HostConf struct {
 	IP   string `yaml:"ip"`
@@ -33,7 +33,7 @@ func init() {
 	Config.Server.Dagent.Port = "3000"
 	Config.Server.IBoF.IP = "127.0.0.1"
 	Config.Server.IBoF.Port = "18716"
-	Config.Server.BMC.IP = "10.1.1.28"
+	Config.Server.BMC.IP = "192.168.0.3"
 	Config.Server.BMC.Port = "443"
 }
 
@@ -54,6 +54,8 @@ func loadSeverConfig() {
 			log.Fatalf("loadSeverConfig Error : %v", err)
 		} else {
 			log.Printf("Open Success : %s", filename)
+			log.Printf("ConfigConfigConfigConfigConfigConfigConfigConfigConfigConfigConfig Success : %+v", Config)
+
 		}
 	}
 }
