2a1e4fa35886cbac1b113e9363d30cb679bc1bdf
root <root@ibof-agent.sys.local>
2019-11-18 16:40:18 +0900

    fix the path of test script.

--- postman/run_test.sh
+++ postman/run_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-newman run sample_test.json -e Test.postman_environment.json -r junit --reporter-junit-export ./result.xml
+newman run test/sample_test.json -e Test.postman_environment.json -r junit --reporter-junit-export ./result.xml
 
 exit 0
