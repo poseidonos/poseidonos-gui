575793c0787a7ab96f2b9029273a29edb9efeb58
d.moon <d.moon@samsung.com>
2019-12-11 17:10:22 +0900

    Fixed run script
    Former-commit-id: b1f5c0144b9c5fbeefd2bbdbd500438eb598a8f7

--- bin/run_ibofos.sh
+++ bin/run_ibofos.sh
@@ -3,3 +3,6 @@
 # run_ibofos.sh
 #
 sudo nohup /root/workspace/ibofos/script/run_ibofos.sh &
+sleep 3
+sudo /root/workspace/ibofos/lib/spdk-19.01.1/scripts/rpc.py nvmf_subsystem_create nqn.2019-04.ibof:subsystem1 -a -s IBOF00000000000001
+sudo /root/workspace/ibofos/lib/spdk-19.01.1/scripts/rpc.py construct_malloc_bdev -b uram0 1024 512
