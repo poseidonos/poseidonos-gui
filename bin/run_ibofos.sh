094d3187b427c3e86c7ff77e1aea73783150d786
d.moon <d.moon@samsung.com>
2019-12-12 16:50:05 +0900

    Rollback run_ibofos.sh
    Former-commit-id: f3f545b0d1acb7ac446ab4f250e4acea35dc3e4c

--- bin/run_ibofos.sh
+++ bin/run_ibofos.sh
@@ -2,7 +2,22 @@
 # 
 # run_ibofos.sh
 #
-sudo nohup /root/workspace/ibofos/script/run_ibofos.sh &
-sleep 3
-sudo /root/workspace/ibofos/lib/spdk-19.01.1/scripts/rpc.py nvmf_subsystem_create nqn.2019-04.ibof:subsystem1 -a -s IBOF00000000000001
-sudo /root/workspace/ibofos/lib/spdk-19.01.1/scripts/rpc.py construct_malloc_bdev -b uram0 1024 512
+logfile="ibofos.log"
+root_dir=/root/workspace/ibofos
+ibofos=${root_dir}/bin/ibofos
+
+if [ ! -f "$ibofos" ]; then
+        echo "fail to find $ibofos. run make prior to run run_ibofos.sh"
+else
+#       ${root_dir}/script/mtool/setup_env.sh
+        \rm -rf /dev/shm/ibof_nvmf_trace*
+#       ${root_dir}/script/mtool/cpumode.sh max
+        nohup $ibofos primary &>> ${logfile} &
+        sleep 2
+	sudo ${root_dir}/lib/spdk-19.01.1/scripts/rpc.py nvmf_subsystem_create nqn.2019-04.ibof:subsystem1 -a -s IBOF00000000000001
+	sudo ${root_dir}/lib/spdk-19.01.1/scripts/rpc.py nvmf_create_transport -t RDMA -u 131072 -p 4 -c 0
+	sudo ${root_dir}/lib/spdk-19.01.1/scripts/rpc.py nvmf_subsystem_add_listener nqn.2019-04.ibof:subsystem1 -t RDMA -a 172.16.1.1 -s 1158
+        sudo ${root_dir}/lib/spdk-19.01.1/scripts/rpc.py construct_malloc_bdev -b uram0 1024 512 
+fi
+echo "finish run_ibofos.sh"
+
