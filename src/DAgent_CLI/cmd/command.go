3804b2fbd8ceb40a99a5698110aea4b2f53e7c5d
hs0112.moon <hs0112.moon@samsung.com>
2020-02-14 00:08:47 -0500

    add help message and fix some errors.
    Former-commit-id: ef8aee29a92a6be494bd90509f2c59713d46856e

--- src/DAgent_CLI/cmd/command.go
+++ src/DAgent_CLI/cmd/command.go
@@ -3,13 +3,9 @@ package cmd
 import (
 		"fmt"
 		"time"
-		//"bytes"
-		//"io/ioutil"
-		//"net/http"
 		"github.com/spf13/cobra"
 		"A-module/log"
-		"errors"
-		//"A-module/errors"
+		"A-module/errors"
 		"A-module/handler"
 		"A-module/setting"
 		iBoFOSV1 "A-module/routers/mtool/api/ibofos/v1"
@@ -44,20 +40,29 @@ var commands = map[string]func() {
 }
 
 var commandCmd = &cobra.Command{
-  Use:   "command",
+  Use:   "command [msg]",
   Short: "Command to Poseidon OS",
-  Long:  `Command to Poseidon OS and get a reply fommated by JSON`,
+  Long:  `Command to Poseidon OS and get a reply fommated by JSON.
+
+available msg list :
+
+[category] : [msg]
+array      : listarray, loadarray, createarray, deletearray
+device     : scandevice, listdevice, attachdevice, detachdevice
+system     : heartbeat,exitibofos, info, mountibof, unmountibof
+volume     : create, update, mountvolume, unmountvolume, delete, list
+	  `,
   Args: func(cmd *cobra.Command, args []string) error {
 
 	if len(args) != 1 {
-      return errors.New("need an one args !!!")
+      return errors.New("need an one msg !!!")
     }
 
 	val, exists := commands[args[0]]
 	_ =  val
 
 	if !exists {
-		return errors.New("not available command!!!")
+		return errors.New("not available msg !!!")
 	}
 
 	return nil
@@ -72,11 +77,13 @@ func init() {
 	rootCmd.AddCommand(commandCmd)
 	commandCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "verbose output")
 	commandCmd.PersistentFlags().BoolVarP(&Debug, "debug", "d", false, "set a debug mode")
+	commandCmd.SetArgs([]string{"sub", "arg1", "arg2"})
+	//commandCmd.SetHelpTemplate("moon hyun suk")
 }
 
 func Send(command string) {
 
-	fmt.Println("command you want to send ", command)
+	fmt.Println("msg you want to send : ", command)
 
 	if Verbose == true {
 		log.SetVerboseMode()
@@ -91,5 +98,4 @@ func Send(command string) {
 	time.Sleep(time.Second*1)
 
 	commands[command]()
-
 }
--- src/DAgent_CLI/cmd/root.go
+++ src/DAgent_CLI/cmd/root.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+		"fmt"
 		"os"
 		"github.com/spf13/cobra"
 	   )
@@ -10,18 +11,14 @@ var rootCmd = &cobra.Command{
 	Short: "DAgent CLI is CLI for DAgent.",
 	Long: `DAgent CLI is CLI for DAgent.`,
 	Run: func(cmd *cobra.Command, args []string) {
-			   // Do Stuff Here
-		   },
+		if len(args) == 0 {
+			fmt.Println("input a command. if you need to get some help, input \"help\".")
+		}
+	},
 }
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
-}
-
-func init() {
-	//rootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "verbose output")
-	//rootCmd.PersistentFlags().BoolVarP(&Debug, "debug", "d", false, "set a debug mode")
-	//rootCmd.PersistentFlags().BoolVarP(&Send, "send", "s", false, "send a command")
 }
