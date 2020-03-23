4a79cec2fab2ddefd4a5b19296ca8302659f6d90
d.moon <d.moon@samsung.com>
2020-03-24 08:23:30 +0900

    Revert "Update vendor"
    This reverts commit 0a5808601b74306711b66031d3ac6dff1bada21c.
    
    
    Former-commit-id: 99f66df78e9788a1350f34f6ff4c5a0096ebbc1b

--- DAgent/vendor/A-module/routers/mtool/api/dagent/v1/execCommand.go
+++ DAgent/vendor/A-module/routers/mtool/api/dagent/v1/execCommand.go
@@ -2,32 +2,26 @@ package v1
 
 import (
 	iBoFOSV1 "A-module/routers/mtool/api/ibofos/v1"
-	"A-module/routers/mtool/model"
 	"A-module/util"
 )
 
-func RunIBoF() (model.Response, error) {
-	res := model.Response{}
-
+func RunIBoF() error {
 	if util.IsIBoFRun() == true {
-		res.Result.Status.Code = 27756
-		return res, iBoFOSV1.ErrRes
+		//return errors.New("exec Run: The iBoFOS already run")
+		return iBoFOSV1.ErrRes
 	}
 
 	util.RunScript("./run_ibofos.sh", false)
 
 	if util.IsIBoFRun() == false {
-		res.Result.Status.Code = 27757
-		return res, iBoFOSV1.ErrRes
+		//return errors.New("exec Run: Fail to run iBoFOS")
+		return iBoFOSV1.ErrRes
 	}
 
-	res.Result.Status.Code = 0
-	return res, nil
+	return nil
 }
 
-func ForceKillIbof() (model.Response, error) {
+func ForceKillIbof() error {
 	util.ExecCmd("pkill -9 ibofos", false)
-	res := model.Response{}
-	res.Result.Status.Code = 0
-	return res, nil
+	return nil
 }
