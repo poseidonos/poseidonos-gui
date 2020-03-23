727ca91395964582bdf0b8f2ac3d4b7ce92bd273
d.moon <d.moon@samsung.com>
2020-03-23 11:42:21 +0900

    Refactor : Use Requester instead of function
    Former-commit-id: 75cd9819f504823980bedc71e1f57f2e71b1ba97

--- A-module/routers/mtool/api/ibofos/v1/volume.go
+++ A-module/routers/mtool/api/ibofos/v1/volume.go
@@ -5,52 +5,25 @@ import (
 )
 
 func CreateVolume(xrId string, param model.VolumeParam) (model.Request, model.Response, error) {
-	return postVolume(xrId, "CREATEVOLUME", param)
+	return Requester{xrId, param}.Post("CREATEVOLUME")
 }
 
 func UpdateVolume(xrId string, param model.VolumeParam) (model.Request, model.Response, error) {
-	return putVolume(xrId, "UPDATEVOLUMEQOS", param)
+	return Requester{xrId, param}.Put("UPDATEVOLUMEQOS")
 }
 
-func MountVolume(xrId string, param model.VolumeParam) (model.Request, model.Response, error)  {
-	return postVolume(xrId, "MOUNTVOLUME", param)
+func MountVolume(xrId string, param model.VolumeParam) (model.Request, model.Response, error) {
+	return Requester{xrId, param}.Post("MOUNTVOLUME")
 }
 
 func UnmountVolume(xrId string, param model.VolumeParam) (model.Request, model.Response, error) {
-	return postVolume(xrId, "UNMOUNTVOLUME", param)
+	return Requester{xrId, param}.Delete("UPDATEVOLUMEQOS")
 }
 
 func DeleteVolume(xrId string, param model.VolumeParam) (model.Request, model.Response, error) {
-	return deleteVolume(xrId, "DELETEVOLUME", param)
+	return Requester{xrId, param}.Delete("DELETEVOLUME")
 }
 
 func ListVolume(xrId string, param model.VolumeParam) (model.Request, model.Response, error) {
-	return getVolume(xrId, "LISTVOLUME")
-}
-
-func getVolume(xrId string, command string) (model.Request, model.Response, error) {
-	iBoFRequest := makeRequest("", command)
-	res, err := sendIBoF(iBoFRequest)
-	return iBoFRequest, res, err
-}
-
-func postVolume(xrId string, command string, param model.VolumeParam) (model.Request, model.Response, error) {
-	iBoFRequest := makeRequest(xrId, command)
-	iBoFRequest.Param = param
-	res, err := sendIBoF(iBoFRequest)
-	return iBoFRequest, res, err
-}
-
-func putVolume(xrId string, command string, param model.VolumeParam) (model.Request, model.Response, error) {
-	iBoFRequest := makeRequest(xrId, command)
-	iBoFRequest.Param = param
-	res, err := sendIBoF(iBoFRequest)
-	return iBoFRequest, res, err
-}
-
-func deleteVolume(xrId string, command string, param model.VolumeParam) (model.Request, model.Response, error) {
-	iBoFRequest := makeRequest(xrId, command)
-	iBoFRequest.Param = param
-	res, err := sendIBoF(iBoFRequest)
-	return iBoFRequest, res, err
+	return Requester{xrId, param}.Get("LISTVOLUME")
 }
