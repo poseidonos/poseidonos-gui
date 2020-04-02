b0f5fb016e2311bce755dd6c22a73aa1ad50d9f9
d.moon <d.moon@samsung.com>
2020-04-02 14:40:24 +0900

    Added stub of tui
    Former-commit-id: c6994d6aa0330808797c5979772dda4df9ed7930

--- tui/main.go
+++ tui/main.go
@@ -13,6 +13,10 @@ import (
 	"github.com/gizak/termui/v3/widgets"
 )
 
+func addWidget() {
+
+}
+
 func main() {
 	if err := ui.Init(); err != nil {
 		log.Fatalf("failed to initialize termui: %v", err)
