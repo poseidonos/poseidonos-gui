70b83bcded91edf1f60dcd0e13bc792dd3be9ceb
hs0112.moon <hs0112.moon@samsung.com>
2020-04-21 14:14:10 +0900

    add a 3000 delay between tests.
    Former-commit-id: ee14810de3a2b1ab5bd46864d9573d822a2adfb0

--- DAgent/test_sol.pe/run_test.sh
+++ DAgent/test_sol.pe/run_test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-newman run base_test.json -e D-Agent.postman_environment.json -r junit --reporter-junit-export result.xml
+ROOT_DIR=$(readlink -f $(dirname $0))/
+cd $ROOT_DIR
+
+newman run --delay-request 3000 base_test.json -e D-Agent.postman_environment.json -r junit --reporter-junit-export result.xml
 
 exit 0
