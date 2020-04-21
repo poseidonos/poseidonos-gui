cbc9837362759bdbaa819679cf957ea869258df8
hs0112.moon <hs0112.moon@samsung.com>
2020-04-21 15:06:51 +0900

    change the vm password.
    Former-commit-id: 1dd3864c146e2a81422898880672b7028ade8d84

--- DAgent/bin/run_remote_ibofos.sh
+++ DAgent/bin/run_remote_ibofos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sshpass -pseb ssh -o StrictHostKeyChecking=no root@$1 "bash -s" < ./run_ibofos.sh
+sshpass -pibof ssh -o StrictHostKeyChecking=no root@$1 "bash -s" < ./run_ibofos.sh
 
 exit 0
