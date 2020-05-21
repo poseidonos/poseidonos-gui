ed4f853491756086a0475aad2e7380ab8b3c3450
hs0112.moon <hs0112.moon@samsung.com>
2020-05-21 11:15:36 +0900

    update new run_remote_ibofos.sh
    Former-commit-id: b9543ba5565d7d81e1217abea0d0f9fd2e245040

--- cli/bin/run_remote_ibofos.sh
+++ cli/bin/run_remote_ibofos.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
-ROOT_DIR=$(readlink -f $(dirname $0))/
-cd $ROOT_DIR
-
-sshpass -pseb ssh -o StrictHostKeyChecking=no root@$1 "bash -s" < ../../DAgent/bin/run_ibofos.sh
+SCRIPT_PATH=$(dirname $(realpath $0))
+sshpass -pseb ssh -o StrictHostKeyChecking=no root@$1 "bash -s" < $SCRIPT_PATH/run_ibofos.sh
 
 exit 0
