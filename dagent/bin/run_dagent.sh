4868f2c071cab5a25741e9e59f55df8ec607ae5c
d.moon <d.moon@samsung.com>
2020-06-05 15:20:03 +0900

    Fixed script
    Former-commit-id: 7a3513b90c47e1107ddb4e1258c3bfa4c1919fad

--- dagent/bin/run_dagent.sh
+++ dagent/bin/run_dagent.sh
@@ -5,8 +5,8 @@ LOG_FILE="/etc/ibofos/log/DAgent.log"
 if [ ! -f "$SCRIPT_PATH/DAgent" ]; then
 	echo "Fail to find executable file."
 else
-	sudo pkill -9 DAgent
-	sudo nohup $SCRIPT_PATH/DAgent &>> ${LOG_FILE} &
+	sudo pkill -9 dagent
+	sudo nohup $SCRIPT_PATH/dagent &>> ${LOG_FILE} &
 	echo "D-Agent is running in background."
 	echo "Run Path : ${SCRIPT_PATH}"
 	echo "Log File : ${LOG_FILE}"
