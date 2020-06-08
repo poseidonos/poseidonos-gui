4d917b201216199b7cfbf8843b57bfc40cefb301
hs0112.moon <hs0112.moon@samsung.com>
2020-06-08 10:53:53 +0900

    change nagios outpur format for multiple volume.
    Former-commit-id: 1fbf9f4abce2c7bd8481e8bc160735653a897273

--- cli/cmd/nagios.go
+++ cli/cmd/nagios.go
@@ -4,6 +4,7 @@ import (
 	_ "a-module/routers/m9k/model"
 	"encoding/json"
 	"fmt"
+	"strconv"
 	"github.com/spf13/cobra"
 	"github.com/tidwall/gjson"
 )
@@ -24,19 +25,32 @@ var nagiosCmd = &cobra.Command{
 func init() {
 
 	rootCmd.AddCommand(nagiosCmd)
-
 }
 
 func PrintVol(cmd *cobra.Command, args []string) {
 
 	command := make([]string, 1)
 	command[0] = "list_vol"
+	isQuiet = true
 
 	res, _ := Send(cmd, command)
 
 	if res.Result.Data != nil {
 		b, _ := json.Marshal(res.Result.Data)
-		value := gjson.Get(string(b), "volumes")
-		fmt.Println(value.String())
+
+		if string(b) == "null" {
+			fmt.Println("CRITICAL: Fail to get the size of volumes")
+		} else {
+			result := "OK: Success |"
+			
+			for i :=0 ; i < int(gjson.Get(string(b),"volumes.#").Int()) ; i++ {
+				
+				value := gjson.Get(string(b), "volumes." + strconv.Itoa(i)).String()
+				
+				result += "'" + gjson.Get(value, "name").String() + "'=" + gjson.Get(value, "total").String() + "B;" 
+			}
+			
+			fmt.Println(result[:len(result)-1])
+		}
 	}
 }
