7335ea371f5ed5064ea25bdfcb68d38e026290fc
hs0112.moon <hs0112.moon@samsung.com>
2020-06-16 09:05:20 +0900

    add dscert file and run docker as priviledged.
    Former-commit-id: 6b786517fb8289df4141af1c2b9ea8a9d1ffd28e

--- docker/build_run.sh
+++ docker/build_run.sh
@@ -1,2 +1,2 @@
 docker build -t ibofos:1.0.0 .
-docker run -it -v /usr/src:/usr/src -v /lib/modules:/lib/modules ibofos:1.0.0 /bin/bash
+docker run -it --cap-add=ALL --privileged -v /usr/src:/usr/src -v /lib/modules:/lib/modules -v /sys:/sys -v /dev:/dev ibofos:1.0.0 /bin/bash
