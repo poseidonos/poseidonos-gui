f042c526ddae5783cbe1a715c54f7a3057d821b9
d.moon <d.moon@samsung.com>
2020-07-03 09:50:11 +0900

    Buidscript move to magent root due to CI
    Former-commit-id: 80ce35fa65b424f7e1e7032a816c5b1f65a60d4b

--- magent/build_magent.sh
+++ magent/build_magent.sh
@@ -3,16 +3,6 @@
 ROOT_DIR=$(readlink -f $(dirname $0))/
 cd $ROOT_DIR
 
-#cd ../a-module/util
-#../bin/go-bindata -o resource.go  -pkg util ../resources/
-#cd $ROOT_DIR
-
-#rm -rf vendor/a-module
-#cp -rf ../a-module ./vendor/
-
-#export GIT_COMMIT_MAGENT=$(git rev-list -1 HEAD)
-#export BUILD_TIME_MAGENT=$(date +%s)
-
-go build -mod vendor 
+go build -mod vendor
 
 mv magent ./bin
