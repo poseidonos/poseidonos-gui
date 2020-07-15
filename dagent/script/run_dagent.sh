c57f0c5f8cb4fa1eb2aa0a6de5b71d6b942820d1
d.moon <d.moon@samsung.com>
2020-07-15 17:20:48 +0900

    Added service for d-agent
    Former-commit-id: cf4ad5fede52945c2eb4598ed2bc08ec7b33b47f

---
+++ dagent/script/run_dagent.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+currdir=$(readlink -f $(dirname $0))
+cd $currdir
+parentdir="$(dirname "$currdir")"
+x=`systemctl is-active dagent.service`
+
+if [ $x = "active" ]
+then
+  echo DAgent service is already running!
+else
+	#create a soft link of the directory, to get the absolute path for starting the service
+	sudo rm /usr/local/dagent
+	sudo ln -s $parentdir /usr/local
+
+	#move the service file  to the /etc/systemd/system/
+	sudo cp $PWD/dagent.service /etc/systemd/system
+	sudo chmod 664 /etc/systemd/system/dagent.service
+
+	sudo systemctl daemon-reload
+	sudo systemctl enable dagent.service
+	sudo systemctl start dagent
+
+	echo "Starting DAgent...."
+	sleep 8s
+	echo "Checking Status..."
+
+	x=`systemctl is-active magent.service`
+	if [ $x = "active" ]
+	then
+  	echo DAgent Service Started Succesfully
+	else
+		sleep 90s
+		if [ $x = "active" ]
+		then
+			echo DAgent Service Started Successfully
+		else
+      DAgent MAgent Service Failed to Start!
+		fi
+	fi
+fi
