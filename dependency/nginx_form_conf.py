731e81a3125f3b50ad86b6436dd11b75e362d529
root <aswin.kk@samsung.com>
2020-07-17 14:39:01 +0900

    NGINX script modification
    Former-commit-id: 1912a3b0fcd1f781a094f4b8917a5b0295a86be7

--- dependency/nginx_form_conf.py
+++ dependency/nginx_form_conf.py
@@ -28,7 +28,7 @@ server {
     }
 }
 """
-L = ["upstream mtool { \n", "     server    {}:5000 weight=50;\n".format(ip), "}\n", "upstream dagent { \n", "     server    {}:3000 weight=50;\n".format(ip), "}\n", "server { \n", "     listen    80;\n", "     server_name    {};\n".format(ip), "     proxy_read_timeout    50000;\n", "     location /api/dagent {\n", "           proxy_pass http://dagent;\n", "    }\n", "     location /api/ibofos {\n", "           proxy_pass http://dagent;\n", "    }\n","     location /api/metric {\n", "           proxy_pass http://dagent;\n", "    }\n",  "     location /redfish {\n", "           proxy_pass http://dagent;\n", "    }\n", "     location ^~ /api/v1.0 {\n", "           proxy_pass http://mtool;\n", "    }\n", "}\n" ]
+L = ["upstream mtool { \n", "     server    {}:5000 weight=50;\n".format(ip), "}\n", "upstream dagent { \n", "     server    {}:3000 weight=50;\n".format(ip), "}\n", "server { \n", "     listen    80;\n", "     server_name    {};\n".format(ip), "     proxy_read_timeout    50000;\n", "location / {\n", "           proxy_pass http://mtool;\n", "    }\n","     location /api/dagent {\n", "           proxy_pass http://dagent;\n", "    }\n", "     location /api/ibofos {\n", "           proxy_pass http://dagent;\n", "    }\n","     location /api/metric {\n", "           proxy_pass http://dagent;\n", "    }\n",  "     location /redfish {\n", "           proxy_pass http://dagent;\n", "    }\n", "     location ^~ /api/v1.0 {\n", "           proxy_pass http://mtool;\n", "    }\n","     location /redfish/v1/StorageServices {\n", "           proxy_pass http://mtool;\n", "    }\n", "}\n" ]
 
 for line in L:
     print(line)
