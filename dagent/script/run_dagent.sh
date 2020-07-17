0064280084526a104ad8e1c6ae062313503656df
d.moon <d.moon@samsung.com>
2020-07-17 18:17:59 +0900

    Modify run script. It always stop the current service and rerun
    Former-commit-id: 3adba675c7ac8cef8f746b23ea59c2e051bd1044

--- dagent/script/run_dagent.sh
+++ dagent/script/run_dagent.sh
@@ -2,39 +2,36 @@
 currdir=$(readlink -f $(dirname $0))
 cd $currdir
 parentdir="$(dirname "$currdir")"
+
+sudo systemctl stop dagent
+
+#create a soft link of the directory, to get the absolute path for starting the service
+sudo rm /usr/local/dagent
+sudo ln -s $parentdir /usr/local
+
+#move the service file  to the /etc/systemd/system/
+sudo cp $PWD/dagent.service /etc/systemd/system
+sudo chmod 664 /etc/systemd/system/dagent.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable dagent.service
+sudo systemctl start dagent
+
+echo "Starting DAgent...."
+sleep 8s
+echo "Checking Status..."
+
 x=`systemctl is-active dagent.service`
 
 if [ $x = "active" ]
 then
-  echo DAgent service is already running!
+  echo DAgent Service Started Succesfully
 else
-	#create a soft link of the directory, to get the absolute path for starting the service
-	sudo rm /usr/local/dagent
-	sudo ln -s $parentdir /usr/local
-
-	#move the service file  to the /etc/systemd/system/
-	sudo cp $PWD/dagent.service /etc/systemd/system
-	sudo chmod 664 /etc/systemd/system/dagent.service
-
-	sudo systemctl daemon-reload
-	sudo systemctl enable dagent.service
-	sudo systemctl start dagent
-
-	echo "Starting DAgent...."
-	sleep 8s
-	echo "Checking Status..."
-
-	x=`systemctl is-active dagent.service`
-	if [ $x = "active" ]
-	then
-  	echo DAgent Service Started Succesfully
-	else
-		sleep 90s
-		if [ $x = "active" ]
-		then
-			echo DAgent Service Started Successfully
-		else
-                        echo DAgent Service Failed to Start!
-		fi
-	fi
+  sleep 90s
+  if [ $x = "active" ]
+  then
+    echo DAgent Service Started Successfully
+  else
+    echo DAgent Service Failed to Start!
+  fi
 fi
--- magent/script/run_magent.sh
+++ magent/script/run_magent.sh
@@ -2,50 +2,46 @@
 currdir=$(readlink -f $(dirname $0))
 cd $currdir
 parentdir="$(dirname "$currdir")"
-x=`systemctl is-active magent.service`
 
+sudo systemctl stop magent
+
+#create a soft link of the directory, to get the absolute path for starting the service
+sudo service influxdb stop
+. ./change_influx_conf.sh
+sudo service influxdb start
+echo "Starting InfuxDB.."
+sleep 8s
+[ ! -d "/tmp/air_result.json" ] && touch /tmp/air_result.json
+[ ! -d "/etc/ibofos" ] && mkdir /etc/ibofos
+[ ! -d "/etc/ibofos/report" ] && mkdir /etc/ibofos/report
+touch /etc/ibofos/report/report.log
+sudo python3 ./create_retention_policy.py
+sudo chown -R influxdb:influxdb /var/lib/influxdb
+sudo rm /usr/local/magent
+sudo ln -s $parentdir /usr/local
+
+#move the service file  to the /etc/systemd/system/
+sudo cp $PWD/magent.service /etc/systemd/system
+sudo chmod 664 /etc/systemd/system/magent.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable magent.service
+sudo systemctl start magent
+
+echo "Starting MAgent...."
+sleep 8s
+echo "Checking Status..."
+
+x=`systemctl is-active magent.service`
 if [ $x = "active" ]
 then
-        echo MAgent service is already running!
+  echo MAgent Service Started Succesfully
 else
-	#create a soft link of the directory, to get the absolute path for starting the service
-	sudo service influxdb stop
-	. ./change_influx_conf.sh
-	sudo service influxdb start
-	echo "Starting InfuxDB.."
-	sleep 8s
-	[ ! -d "/tmp/air_result.json" ] && touch /tmp/air_result.json
-	[ ! -d "/etc/ibofos" ] && mkdir /etc/ibofos
-	[ ! -d "/etc/ibofos/report" ] && mkdir /etc/ibofos/report
-	touch /etc/ibofos/report/report.log
-	sudo python3 ./create_retention_policy.py
-	sudo chown -R influxdb:influxdb /var/lib/influxdb
-	sudo rm /usr/local/magent
-	sudo ln -s $parentdir /usr/local
-
-	#move the service file  to the /etc/systemd/system/
-	sudo cp $PWD/magent.service /etc/systemd/system
-	sudo chmod 664 /etc/systemd/system/magent.service
-
-	sudo systemctl daemon-reload
-	sudo systemctl enable magent.service
-	sudo systemctl start magent
-
-	echo "Starting MAgent...."
-	sleep 8s
-	echo "Checking Status..."
-
-	x=`systemctl is-active magent.service`
-	if [ $x = "active" ]
-	then
-  	echo MAgent Service Started Succesfully
-	else
-		sleep 90s
-		if [ $x = "active" ]
-		then
-			echo MAgent Service Started Successfully
-		else
-        		echo MAgent Service Failed to Start!
-		fi
-	fi
+  sleep 90s
+  if [ $x = "active" ]
+  then
+    echo MAgent Service Started Successfully
+  else
+          echo MAgent Service Failed to Start!
+  fi
 fi
