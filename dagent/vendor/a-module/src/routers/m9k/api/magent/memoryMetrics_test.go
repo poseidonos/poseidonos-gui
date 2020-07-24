748a223176ef061d1b7363a7e9596c9d5acafc5f
d.moon <d.moon@samsung.com>
2020-07-24 15:05:11 +0900

    Update vendor
    Former-commit-id: 6252f16b60156663ef8c35b4d212af63d5f73905

---
+++ dagent/vendor/a-module/src/routers/m9k/api/magent/memoryMetrics_test.go
@@ -0,0 +1,86 @@
+package magent
+
+import (
+	"a-module/src/routers/m9k/api/magent/mocks"
+	"a-module/src/routers/m9k/model"
+	"encoding/json"
+	"reflect"
+	"testing"
+)
+
+func TestGetMemoryData(t *testing.T) {
+	var tests = []struct {
+		input    model.MAgentParam
+		expected interface{}
+		err      error
+	}{
+		{
+			input: model.MAgentParam{},
+			expected: MemoryFields{
+				{
+					UsageUser: json.Number("70"),
+					Time:      "1589872050483860738",
+				},
+			},
+			err: nil,
+		},
+		{
+			input: model.MAgentParam{
+				Time: "15m",
+			},
+			expected: MemoryFields{
+				{
+					UsageUser: json.Number("50"),
+					Time:      json.Number("1589872050483860738"),
+				},
+				{
+					UsageUser: json.Number("40"),
+					Time:      json.Number("1589872050483870738"),
+				},
+			},
+			err: nil,
+		},
+	}
+
+	IDBClient = mocks.MockInfluxClient{}
+	for _, test := range tests {
+		result, err := GetMemoryData(test.input)
+		output := result.Result.Data
+		if !reflect.DeepEqual(output, test.expected) || err != test.err {
+			t.Errorf("Test Failed: %v inputted, %v expected, received: %v, received err: %v", test.input, test.expected, output, err)
+		}
+	}
+
+}
+
+func TestGetMemoryDataError(t *testing.T) {
+	var tests = []struct {
+		input    model.MAgentParam
+		expected int
+		err      error
+	}{
+		{
+			input: model.MAgentParam{
+				Time: "115m", //incorrect param
+			},
+			expected: 500,
+			err:      nil,
+		},
+		{
+			input: model.MAgentParam{
+				Time: "60d", //no data
+			},
+			expected: 500,
+			err:      nil,
+		},
+	}
+
+	IDBClient = mocks.MockInfluxClient{}
+	for _, test := range tests {
+		result, err := GetMemoryData(test.input)
+		output := result.Result.Status.Code
+		if !reflect.DeepEqual(output, test.expected) || err != test.err {
+			t.Errorf("Test Failed: %v inputted, %v expected, received: %v, received err: %v", test.input, test.expected, output, err)
+		}
+	}
+}
