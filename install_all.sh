e2052cfd31ce0d42fed8136efd9d1b0b338b85d2
d.moon <d.moon@samsung.com>
2020-08-06 17:00:03 +0900

    Change script
    Former-commit-id: 0d2dbe02b2f21ed292be22b039911caa4590dcbf

--- install_all.sh
+++ install_all.sh
@@ -3,6 +3,6 @@
 SCRIPT_PATH=$(readlink -f $(dirname $0))
 
 $SCRIPT_PATH/dependency/install.sh nas
-#$SCRIPT_PATH/dagent/dependency/install.sh
-$SCRIPT_PATH/magent/dependency/install.sh nas
+$SCRIPT_PATH/dagent/dependency/install.sh
+$SCRIPT_PATH/magent/dependency/install.sh
 $SCRIPT_PATH/mtool/dependency/install.sh nas
--- mtool/dependency/install.sh
+++ mtool/dependency/install.sh
@@ -1,39 +1,20 @@
 #!/bin/bash
 
-ROOT_DIR=$(readlink -f $(dirname $0))
-cd $ROOT_DIR
+SCRIPT_PATH=$(readlink -f $(dirname $0))
 
 if [ $1 ]
 then
-if [ $1 = "nas" ]
-then
-  wget -O /tmp/ibofmgmt_package.tar.gz  http://10.1.5.22/mtool.packages/ibofmgmt_package.tar.gz
-  cd /tmp
-  chmod 777 ibofmgmt_package.tar.gz
-  tar -xzvf ibofmgmt_package.tar.gz
-  chmod 777 -R *
-  echo "DONE"
-  cd $ROOT_DIR
-  sudo -H pip3  install --no-index --find-links=/tmp/ibofmgmt_package/python-packages/ -r ../requirements.txt
-  rm /tmp/ibofmgmt_package.tar.gz
-  rm  -r /tmp/ibofmgmt_package
-elif [ $1 = "apt" ]
-then
-  sudo -H pip3  install -r ../requirements.txt
+  if [ $1 = "nas" ]
+  then
+    sudo -H pip3  install --no-index --find-links=$SCRIPT_PATH/python-packages/ -r $SCRIPT_PATH/requirements.txt
+  elif [ $1 = "apt" ]
+  then
+    sudo -H pip3 install -r $SCRIPT_PATH/requirements.txt
+  else
+    echo "Provide nas/apt as argument for installation source"
+    exit 0
+  fi
 else
   echo "Provide nas/apt as argument for installation source"
   exit 0
 fi
-else
-  echo "Provide nas/apt as argument for installation source"
-  exit 0
-fi
-
-#cd $ROOT_DIR
-#Changing retention Policy
-#service influxd stop
-#. ../scripts/change_influx_conf.sh
-#service influxd start
-#sleep 8s
-#sudo python3 ../scripts/create_retention_policy.py
-#sudo chown -R influxdb:influxdb /var/lib/influxdb
