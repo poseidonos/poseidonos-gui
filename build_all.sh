6284c04f36cb3016564c58195f0d8ff7dd3f6c6c
d.moon <d.moon@samsung.com>
2020-08-20 16:28:42 +0900

    Commet out mtool in run_all.sh
    Former-commit-id: 7cf0b6b57348cc33fd063231b0e27db10d320b4a

--- build_all.sh
+++ build_all.sh
@@ -3,7 +3,7 @@
 SCRIPT_PATH=$(readlink -f $(dirname $0))
 
 $SCRIPT_PATH/a-module/script/build_resource.sh
-$SCRIPT_PATH/cli/script/build_cli.sh
 $SCRIPT_PATH/dagent/script/build_dagent.sh
-$SCRIPT_PATH/tui/script/build_tui.sh
 $SCRIPT_PATH/magent/script/build_magent.sh
+$SCRIPT_PATH/cli/script/build_cli.sh
+$SCRIPT_PATH/tui/script/build_tui.sh
