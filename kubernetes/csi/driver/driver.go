47aea338c66b902d0be59c41c3e6216670621a56
hs0112.moon <hs0112.moon@samsung.com>
2020-08-27 16:10:24 +0900

    update csi pkg.
    Former-commit-id: 61540f1f58cba5ed7e7f8d285f8cee6dc3070625

--- kubernetes/csi/driver/driver.go
+++ kubernetes/csi/driver/driver.go
@@ -3,7 +3,7 @@ package driver
 import (
 	_ "fmt"
 	"github.com/container-storage-interface/spec/lib/go/csi"
-	//"google.golang.org/grpc"
+	"google.golang.org/grpc"
 )
 
 func NewDriver(ep, token, url, doTag, driverName, debugAddr string) error {
--- kubernetes/csi/go.mod
+++ kubernetes/csi/go.mod
@@ -5,5 +5,5 @@ go 1.14
 require (
 	github.com/container-storage-interface/spec v1.3.0
 	github.com/golang/protobuf v1.4.2 // indirect
-	google.golang.org/grpc v1.31.0 // indirect
+	google.golang.org/grpc v1.31.0
 )
