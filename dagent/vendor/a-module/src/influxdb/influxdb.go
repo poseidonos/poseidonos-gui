3c7cfebd8cf26f0d3f7a8f37548f897d0a10e6cd
hs0112.moon <hs0112.moon@samsung.com>
2020-09-01 16:12:02 +0900

    update vendor dir.
    Former-commit-id: dfc81f907b6135798b77695d0648c7cc300a2d02

---
+++ dagent/vendor/a-module/src/influxdb/influxdb.go
@@ -0,0 +1,34 @@
+package influxdb
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/influxdata/influxdb-client-go/v2"
+)
+
+func Test() {
+	// Create a new client using an InfluxDB server base URL and an authentication token
+	client := influxdb2.NewClient("http://localhost:9999", "my-token")
+	// Use blocking write client for writes to desired bucket
+	writeAPI := client.WriteAPIBlocking("my-org", "my-bucket")
+	// Create point using full params constructor
+	p := influxdb2.NewPoint("stat",
+		map[string]string{"unit": "temperature"},
+		map[string]interface{}{"avg": 24.5, "max": 45},
+		time.Now())
+	// write point immediately
+	writeAPI.WritePoint(context.Background(), p)
+	// Create point using fluent style
+	p = influxdb2.NewPointWithMeasurement("stat").
+		AddTag("unit", "temperature").
+		AddField("avg", 23.2).
+		AddField("max", 45).
+		SetTime(time.Now())
+	writeAPI.WritePoint(context.Background(), p)
+
+	// Or write directly line protocol
+	line := fmt.Sprintf("stat,unit=temperature avg=%f,max=%f", 23.5, 45.0)
+	writeAPI.WriteRecord(context.Background(), line)
+}
