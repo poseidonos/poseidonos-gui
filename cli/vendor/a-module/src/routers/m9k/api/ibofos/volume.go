01aa62f742bb287c5ff8a2e08f2ec24fcdaea336
d.moon <d.moon@samsung.com>
2020-09-07 17:02:53 +0900

    Added json type validation
    Former-commit-id: 7ef50ba70d660dbc2a8c06d0497bc56433014784

--- cli/vendor/a-module/src/routers/m9k/api/ibofos/volume.go
+++ cli/vendor/a-module/src/routers/m9k/api/ibofos/volume.go
@@ -7,11 +7,10 @@ import (
 )
 
 func CreateVolume(xrId string, param interface{}) (model.Request, model.Response, error) {
-
 	var resErr string
 
 	err1 := influxdb.CreateVolume()
-	req, res, err2 := Requester{xrId, param}.Post("CREATEVOLUME")
+	req, res, err2 := Requester{xrId, param, model.VolumeParam{}}.Send("CREATEVOLUME")
 
 	if err1 != nil {
 		resErr = "Influx Error : " + err1.Error()
@@ -29,23 +28,22 @@ func CreateVolume(xrId string, param interface{}) (model.Request, model.Response
 }
 
 func UpdateVolume(xrId string, param interface{}) (model.Request, model.Response, error) {
-	return Requester{xrId, param}.Put("UPDATEVOLUMEQOS")
+	return Requester{xrId, param,model.VolumeParam{}}.Send("UPDATEVOLUMEQOS")
 }
 
 func MountVolume(xrId string, param interface{}) (model.Request, model.Response, error) {
-	return Requester{xrId, param}.Post("MOUNTVOLUME")
+	return Requester{xrId, param,model.VolumeParam{}}.Send("MOUNTVOLUME")
 }
 
 func UnmountVolume(xrId string, param interface{}) (model.Request, model.Response, error) {
-	return Requester{xrId, param}.Delete("UNMOUNTVOLUME")
+	return Requester{xrId, param,model.VolumeParam{}}.Send("UNMOUNTVOLUME")
 }
 
 func DeleteVolume(xrId string, param interface{}) (model.Request, model.Response, error) {
-
 	var resErr string
 
 	err1 := influxdb.DeleteVolume()
-	req, res, err2 := Requester{xrId, param}.Delete("DELETEVOLUME")
+	req, res, err2 := Requester{xrId, param,model.VolumeParam{}}.Send("DELETEVOLUME")
 
 	if err1 != nil {
 		resErr = "Influx Error : " + err1.Error()
@@ -63,25 +61,29 @@ func DeleteVolume(xrId string, param interface{}) (model.Request, model.Response
 }
 
 func ListVolume(xrId string, param interface{}) (model.Request, model.Response, error) {
-	return Requester{xrId, param}.Get("LISTVOLUME")
+	return volumeSender(xrId, param, "LISTVOLUME")
 }
 
 func UpdateVolumeQoS(xrId string, param interface{}) (model.Request, model.Response, error) {
-	return Requester{xrId, param}.Post("UPDATEVOLUMEQOS")
+	return volumeSender(xrId, param, "UPDATEVOLUMEQOS")
 }
 
 func RenameVolume(xrId string, param interface{}) (model.Request, model.Response, error) {
-	return Requester{xrId, param}.Post("RENAMEVOLUME")
+	return volumeSender(xrId, param, "RENAMEVOLUME")
 }
 
 func ResizeVolume(xrId string, param interface{}) (model.Request, model.Response, error) {
-	return Requester{xrId, param}.Post("RESIZEVOLUME")
+	return volumeSender(xrId, param, "RESIZEVOLUME")
 }
 
 func GetMaxVolumeCount(xrId string, param interface{}) (model.Request, model.Response, error) {
-	return Requester{xrId, param}.Get("GETMAXVOLUMECOUNT")
+	return volumeSender(xrId, param, "GETMAXVOLUMECOUNT")
 }
 
 func GetHostNQN(xrId string, param interface{}) (model.Request, model.Response, error) {
-	return Requester{xrId, param}.Get("GETHOSTNQN")
+	return volumeSender(xrId, param, "GETHOSTNQN")
+}
+
+func volumeSender(xrId string, param interface{}, command string) (model.Request, model.Response, error) {
+	return Requester{xrId, param, model.VolumeParam{}}.Send(command)
 }
