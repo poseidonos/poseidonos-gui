561c76d458fa5ca11f66ee11644cd177d60b3d98
Aswin K K <aswin.kk@samsung.com>
2020-10-06 08:24:04 +0530

    Updated Help File
    Former-commit-id: 995f8aa2d0433d7935c690cf0b33198beacc070f

---
+++ mtool/public/precache-manifest.ec14c5105735eac6c1a3ce6d838b754f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0757d6421d513ecf2727b6a855041d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a14dc3fa2c1db617be5",
+    "url": "/static/css/2.06a9ef75.chunk.css"
+  },
+  {
+    "revision": "94155c5a48941ee0bd0f",
+    "url": "/static/css/main.73af2b61.chunk.css"
+  },
+  {
+    "revision": "3a14dc3fa2c1db617be5",
+    "url": "/static/js/2.d4e48c2c.chunk.js"
+  },
+  {
+    "revision": "3e30ee04c38dfb236e979cf9d68639e7",
+    "url": "/static/js/2.d4e48c2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94155c5a48941ee0bd0f",
+    "url": "/static/js/main.fe93c3b6.chunk.js"
+  },
+  {
+    "revision": "ef844570c74027621af6",
+    "url": "/static/js/runtime-main.a3e533b7.js"
+  },
+  {
+    "revision": "1e52dde6087fa06b44d8caa0d3a79b62",
+    "url": "/static/media/Header-logo.1e52dde6.png"
+  },
+  {
+    "revision": "7b3551577c134279af60786b3f77923b",
+    "url": "/static/media/Samsung_iBOF_Management_Tool_User_Manual.7b355157.pdf"
+  }
+]);
--- mtool/public/service-worker.js
+++ mtool/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e4fae42a55261cd4e37fd4e976dd5093.js"
+  "/precache-manifest.ec14c5105735eac6c1a3ce6d838b754f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
