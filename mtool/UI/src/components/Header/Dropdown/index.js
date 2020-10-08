4f3c56e7cc33cfbee49fd7439c4a8cd3baff0c5f
Palak Kapoor <palak.k@samsung.com>
2020-10-08 12:31:05 +0530

    Specifying the MIME type of Help file
    Former-commit-id: 49c2298a089cb40a98f55f484cd249c00bde79ee

--- mtool/UI/src/components/Header/Dropdown/index.js
+++ mtool/UI/src/components/Header/Dropdown/index.js
@@ -63,7 +63,7 @@ const Dropdown = (props) => {
       <MenuItem className={classes.menuItem}><NavLink to="/ConfigurationSetting/user">User Management</NavLink></MenuItem>
       <Divider />
       <MenuItem className={classes.menuItem}>
-        <a href={PDF} target="_blank" className={classes.optionItem} rel="noopener noreferrer">Help</a>
+        <a href={PDF} target="_blank" type="application/pdf" className={classes.optionItem} rel="noopener noreferrer">Help</a>
       </MenuItem>
       <Divider />
       {/* <MenuItem className={classes.menuItem} onClick={props.renderPopup}>Change Password</MenuItem>
