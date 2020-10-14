9ba01b61ea2b9e38dd3a48d50f1b411fa6b08291
hs0112.moon <hs0112.moon@samsung.com>
2020-10-14 14:32:50 +0900

    Fixed script
    Former-commit-id: 54b441a50856eaf0ed3e7c37993ecef2a53ee282

--- tui/vendor/a-module/src/util/resource.go
+++ tui/vendor/a-module/src/util/resource.go
@@ -92,7 +92,7 @@ func ResourcesEventsYaml() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "../resources/events.yaml", size: 35143, mode: os.FileMode(420), modTime: time.Unix(1602566621, 0)}
+	info := bindataFileInfo{name: "../resources/events.yaml", size: 35143, mode: os.FileMode(420), modTime: time.Unix(1602166107, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
