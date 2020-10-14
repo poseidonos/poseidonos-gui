c8e84e82e92efab215218b8de25febb2ecbf6ec8
d.moon <d.moon@samsung.com>
2020-10-14 09:09:52 +0900

    Update vendor related file
    Former-commit-id: 9591ff9b7dbff16b0fa1a034f2a272191c65cdbc

--- dagent/vendor/a-module/src/util/resource.go
+++ dagent/vendor/a-module/src/util/resource.go
@@ -92,7 +92,7 @@ func ResourcesEventsYaml() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "../resources/events.yaml", size: 35143, mode: os.FileMode(436), modTime: time.Unix(1602137304, 0)}
+	info := bindataFileInfo{name: "../resources/events.yaml", size: 35143, mode: os.FileMode(420), modTime: time.Unix(1602566621, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
