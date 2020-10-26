0875c49ff8784f76bcf475d58210eff7e6aa9131
d.moon <d.moon@samsung.com>
2020-10-26 14:54:33 +0900

    add csi controller code.
    Former-commit-id: 698dbca1aa5d84a187d020853f716f2bf39c8517

--- kubernetes/csi/driver/driver.go
+++ kubernetes/csi/driver/driver.go
@@ -2,9 +2,27 @@ package driver
 
 import (
 	_ "fmt"
-	"github.com/container-storage-interface/spec/lib/go/csi"
+	_ "github.com/container-storage-interface/spec/lib/go/csi"
 	"google.golang.org/grpc"
 )
+
+
+
+type Driver struct {
+    name string
+    // publishInfoVolumeName is used to pass the volume name from
+    // `ControllerPublishVolume` to `NodeStageVolume or `NodePublishVolume`
+    publishInfoVolumeName string
+
+    endpoint          string
+    debugAddr         string
+    hostID            func() string
+    region            string
+    doTag             string
+    isController      bool
+    
+    srv     *grpc.Server
+}
 
 func NewDriver(ep, token, url, doTag, driverName, debugAddr string) error {
 
