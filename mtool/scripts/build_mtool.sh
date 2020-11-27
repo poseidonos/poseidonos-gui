30b6add9bfd45bd5512e653989f8fd282fb3925c
vishal shakya <vishal.s7@samsung.com>
2020-11-27 14:56:12 +0530

    Fixed reset button issue
    Former-commit-id: ee4b776b184d998439e90ea35491afa2a2e3f63f

--- mtool/scripts/build_mtool.sh
+++ mtool/scripts/build_mtool.sh
@@ -6,7 +6,10 @@ cd ..
 rm -r dist
 
 pipenv run pyinstaller --hidden-import=eventlet.hubs.epolls --hidden-import=eventlet.hubs.kqueue --hidden-import=eventlet.hubs.selects --hidden-import=dns.asyncbackend --hidden-import=dns.asyncquery --hidden-import=dns.asyncresolver --hidden-import=dns.e164 --hidden-import=dns.namedict --hidden-import=dns.tsigkeyring --hidden-import=dns.zone --hidden-import=engineio.async_drivers.aiohttp --hidden-import=engineio.async_drivers.eventlet --onefile rest/app.py > /dev/null 2>&1
-
+if [ ! -d "dist/scripts" ]; then
+  mkdir dist/scripts
+fi
+cp scripts/cleanup.sh dist/scripts/
 mkdir dist/bin
 mv dist/app dist/bin
 cp -r public dist
