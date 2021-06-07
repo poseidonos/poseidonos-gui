1157ed2f9a81c4731ed7b0475cbdaa53fe0d40ff
Aswin K K <aswin.kk@samsung.com>
2021-06-07 19:10:44 +0530

    Removed CLI and TUI from build scripts
    Former-commit-id: eb3cf2d62e5f274209ac3c3eb2c0b4fa8455d800

--- build_all.sh
+++ build_all.sh
@@ -5,7 +5,5 @@ SCRIPT_PATH=$(readlink -f $(dirname $0))
 $SCRIPT_PATH/pnconnector/script/build_resource.sh
 $SCRIPT_PATH/dagent/script/build_dagent.sh
 $SCRIPT_PATH/magent/script/build_magent.sh
-$SCRIPT_PATH/cli/script/build_cli.sh
-$SCRIPT_PATH/tui/script/build_tui.sh
 $SCRIPT_PATH/mtool/scripts/build_mtool.sh
 $SCRIPT_PATH/dependency/build_python_scripts.sh
