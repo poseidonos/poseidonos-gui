29014caf33bd001d72b1f6870d7f2d5d1bb29ccd
vishal.s7 <vishal.s7@samsung.com>
2021-06-10 17:58:08 +0530

    Removed CopyRight text from serviceWorker.js
    Former-commit-id: 561580964527daeceb7b5967cf68707b44f1082f

--- src/mtool/ui/src/serviceWorker.js
+++ src/mtool/ui/src/serviceWorker.js
@@ -1,29 +1,3 @@
-/* -------------------------------------------------------------------------------------/
-                                                                                    /
-/               COPYRIGHT (c) 2019 SAMSUNG ELECTRONICS CO., LTD.                      /
-/                          ALL RIGHTS RESERVED                                        /
-/                                                                                     /
-/   Permission is hereby granted to licensees of Samsung Electronics Co., Ltd.        /
-/   products to use or abstract this computer program for the sole purpose of         /
-/   implementing a product based on Samsung Electronics Co., Ltd. products.           /
-/   No other rights to reproduce, use, or disseminate this computer program,          /
-/   whether in part or in whole, are granted.                                         / 
-/                                                                                     /
-/   Samsung Electronics Co., Ltd. makes no representation or warranties with          /
-/   respect to the performance of this computer program, and specifically disclaims   /
-/   any responsibility for any damages, special or consequential, connected           /
-/   with the use of this program.                                                     /
-/                                                                                     /
-/-------------------------------------------------------------------------------------/
-
-
-DESCRIPTION: <File description> *
-@NAME : serviceWorker.js
-@AUTHORS: Jay Hitesh Sanghavi 
-@Version : 1.0 *
-@REVISION HISTORY
-[03/06/2019] [Jay] : Prototyping..........////////////////////
-*/
 // This optional code is used to register a service worker.
 // register() is not called by default.
 
