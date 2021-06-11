0f14f2a2f7ff3842b33a6cab4b1ba3aa60d9e0c4
vishal.s7 <vishal.s7@samsung.com>
2021-06-11 16:32:16 +0530

    Added License text to UI files
    Former-commit-id: b503413b3a204223dd7adbee2cbbb8b5a854eb80

--- src/mtool/ui/src/components/VolumeManagement/VolumeList/index.js
+++ src/mtool/ui/src/components/VolumeManagement/VolumeList/index.js
@@ -1,29 +1,35 @@
-/* -------------------------------------------------------------------------------------/
-                                                                                    /
-/               COPYRIGHT (c) 2019 SAMSUNG ELECTRONICS CO., LTD.                      /
-/                          ALL RIGHTS RESERVED                                        /
-/                                                                                     /
-/   Permission is hereby granted to licensees of Samsung Electronics Co., Ltd.        /
-/   products to use or abstract this computer program for the sole purpose of         /
-/   implementing a product based on Samsung Electronics Co., Ltd. products.           /
-/   No other rights to reproduce, use, or disseminate this computer program,          /
-/   whether in part or in whole, are granted.                                         / 
-/                                                                                     /
-/   Samsung Electronics Co., Ltd. makes no representation or warranties with          /
-/   respect to the performance of this computer program, and specifically disclaims   /
-/   any responsibility for any damages, special or consequential, connected           /
-/   with the use of this program.                                                     /
-/                                                                                     /
-/-------------------------------------------------------------------------------------/
+/*
+ *   BSD LICENSE
+ *   Copyright (c) 2021 Samsung Electronics Corporation
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
-
-DESCRIPTION: <File description> *
-@NAME : index.js
-@AUTHORS: Jay Hitesh Sanghavi 
-@Version : 1.0 *
-@REVISION HISTORY
-[03/06/2019] [Jay] : Prototyping..........////////////////////
-*/
 import React, { Component } from 'react';
 import 'react-dropdown/style.css';
 import PropTypes from 'prop-types';
