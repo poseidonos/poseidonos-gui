c67cd45316f90d95f7039ef65a3b4de8846ec564
vishal.s7 <vishal.s7@samsung.com>
2021-06-24 18:20:26 +0530

    removed print statement
    Former-commit-id: 6ffad03e595d7de8e0f9b7d55af36bf1868cfece

--- src/dagent/src/routers/m9k/api/ibofos/caller.go
+++ src/dagent/src/routers/m9k/api/ibofos/caller.go
@@ -41,14 +41,12 @@ import (
 	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
-	"fmt"
 )
 
 func CalliBoFOS(ctx *gin.Context, f func(string, interface{}) (model.Request, model.Response, error)) {
 	req := model.Request{}
 	ctx.ShouldBindBodyWith(&req, binding.JSON)
 	_, res, err := f(header.XrId(ctx), req.Param)
-	fmt.Println("response >>>>>==========:",res)
 	api.HttpResponse(ctx, res, err)
 }
 
