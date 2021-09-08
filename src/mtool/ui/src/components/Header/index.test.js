b34cffe89af9bc2633a1aa5ad5a8bb8de326a980
Aswin K K <aswin.kk@samsung.com>
2021-09-08 18:49:41 +0530

    Version Updated
    Former-commit-id: 30bd279c7b81d0459f846d444977602ea83d6534

--- src/mtool/ui/src/components/Header/index.test.js
+++ src/mtool/ui/src/components/Header/index.test.js
@@ -101,7 +101,7 @@ describe('<Header />', () => {
   it("should render the correct version and last active time", () => {
     renderComponent();
     const  { getByText } = wrapper;
-    expect(getByText("Version 0.6.0")).toBeDefined();
+    expect(getByText("Version 0.7.0")).toBeDefined();
     expect(getByText(/POS Last Active Time:/)).toBeDefined();
   })
 
