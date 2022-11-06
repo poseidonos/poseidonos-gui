119bbecf22babbe5575d4b259366feaf621b74df
Manoj Kumar Sarkar <manoj22.k@samsung.com>
2022-11-07 02:07:30 +0530

    Update Available volume size in UI if available size is less than 1MB then it will show 0 B

--- src/mtool/ui/src/utils/constants/index.js
+++ src/mtool/ui/src/utils/constants/index.js
@@ -1,4 +1,4 @@
-export const BYTE_FACTOR = 1000;
+export const BYTE_FACTOR = 1024;
 // eslint-disable-next-line no-useless-escape
 export const EMAIL_REGEX = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 export const PHONE_REGEX = /^\+(?:[0-9] ?){6,14}[0-9]$/
