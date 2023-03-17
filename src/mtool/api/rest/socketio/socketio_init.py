68b90b1d6ac7f3d4acf9e62063e3cc9494c0272a
Manoj Kumar Sarkar <111956415+manoj22-k@users.noreply.github.com>
2023-03-17 14:30:58 +0530

    removed semicolon

--- src/mtool/api/rest/socketio/socketio_init.py
+++ src/mtool/api/rest/socketio/socketio_init.py
@@ -1,7 +1,7 @@
 
 from flask_socketio import SocketIO
 
-socketIo = SocketIO();
+socketIo = SocketIO()
 
 def init_socketio(app):
     socketIo.init_app(app, cors_allowed_origins="*", async_mode='eventlet')
