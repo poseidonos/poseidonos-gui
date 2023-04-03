a9ef91a71667f48237714e1ad1445db675434dae
Manoj Kumar Sarkar <manoj22.k@samsung.com>
2023-04-03 12:32:59 +0530

    Kept ipmi-exporter tar file and updated script file

--- dependency/setup_ipmi-exporter.sh
+++ dependency/setup_ipmi-exporter.sh
@@ -14,14 +14,12 @@ then
     echo "Process with PID $pid using port 9290 has been killed"
 fi
 
-wget https://github.com/prometheus-community/ipmi_exporter/releases/download/v1.6.1/ipmi_exporter-1.6.1.linux-amd64.tar.gz
-
 tar xvfz ipmi_exporter-1.6.1.linux-amd64.tar.gz
 
 # add PATH
 mv ipmi_exporter-1.6.1.linux-amd64/ipmi_exporter /usr/local/bin/
 
-rm -r ipmi_exporter-1.6.1.linux-amd64*
+rm -r ipmi_exporter-1.6.1.linux-amd64/
 
 # register ipmi-exporter.service
 cp ipmi-exporter.service /lib/systemd/system/
