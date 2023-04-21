c240674e9aac8efefbcfa7795765c7620e8af783
Aswin K K <aswin.kk@samsung.com>
2023-04-21 14:32:24 +0530

    Version Updated Version Updated in UI

--- src/mtool/ui/src/components/Header/index.test.js
+++ src/mtool/ui/src/components/Header/index.test.js
@@ -99,7 +99,7 @@ describe('<Header />', () => {
   it("should render the correct version", () => {
     renderComponent();
     const { getByText } = wrapper;
-    expect(getByText("v1.0.0-rc0")).toBeDefined();
+    expect(getByText("v1.0.0-rc1")).toBeDefined();
   })
 
   it("should change the Password", async () => {
